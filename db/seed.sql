fb0cfafb34fa44b7fece044987b25a315252a991
Cesar Campos <cesarorlandocampos@gmail.com>
2021-04-19 23:10:06 -0500

    saving point

---
+++ db/seed.sql
@@ -0,0 +1,43 @@
+USE employeedb;
+
+INSERT INTO department(department_name)
+VALUES
+
+('Engineering'), ('Sales'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+
+('Legal Manager', 230000, 1),
+('Lawyer', 180000, 1),
+('Legal', 150000, 1),
+('Engineering Manager', 170000, 2),
+
+('Software Engineer', 100000, 2),
+('Engineer', 90000, 2),
+('Financial Manager', 140000, 3),
+('Financial Analyst', 90000, 3),
+('Analyst', 80000, 3),
+('Sales Manager', 90000, 4),
+('Sales Lead', 70000, 4),
+('Sales', 50000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+
+('Rachel', 'Thiim','1', '1'),
+('Cesar', 'Campos','2', '2'),
+('Melisa', 'Campos','3', '1'),
+('Austin', 'Smith','4', '3'),
+('Joel', 'Campos','5', '4'),
+('Denis', 'Salvino','6', '1'),
+('George', 'Huliaris','7', '2'),
+('Matthew', 'Miller','8', '3'),
+('Michael', 'Galloway','9', '4'),
+('Rodgrigo', 'Galicia','10', '4'),
+('Sean', 'Francis','11', '2'),
+('Susan', 'Fuijii','12', '3');
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
--- server.js
+++ server.js
@@ -1,29 +1,17 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-// const express = require('express');
-// const sequelize = require('./config/connection.js');
-
-// const app = express();
-// const PORT = process.env.PORT || 3306;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// sequelize.sync().then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-// });
+require("dotenv").config
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Gogi5683@',
-    database: 'employeeDB',
+    database: 'employeedb',
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected!");
+    (err) ? console.error(err): console.log("Connected!");
     SystemSearch();
 });
 
@@ -70,3 +58,143 @@ const SystemSearch = () => {
             }
         })
 }
+
+const viewEmployees = () => {
+    connection.query('SELECT * FROM employee', (err, result) => {
+        err ? console.error(err) : console.table(result);
+        SystemSearch();
+    });
+};
+
+const viewDepartments = () => {
+    connection.query('SELECT * FROM department', (err, result) => {
+        err ? console.error(err) : console.table(result);
+        SystemSearch();
+    });
+};
+
+const viewRoles = () => {
+    connection.query('SELECT * FROM role', (err, result) => {
+        err ? console.error(err) : console.table(result);
+        SystemSearch();
+    });
+};
+
+const addDepartment = () => {
+    inquirer.prompt([{
+        name: 'department_name',
+        type: 'input',
+        message: 'Enter the department name:',
+    }]).then(({ department_name }) => {
+        const query = connection.query('INSERT INTO department SET ?', { department_name }, (err, result) => {
+            (err) ? err: console.log('Failed to add Department.')
+            viewDepartments();
+        })
+    })
+};
+
+const addEmployee = () => {
+    connection.query('SELECT role.id AS id, role.title as title FROM role', (err, results) => {
+        err ? console.error(err) : inquirer.prompt([{
+                name: 'firstName',
+                message: "What is the Employee's First Name?",
+            }, {
+                name: 'lastName',
+                message: "What is the Employee's Last Name?",
+            }, {
+                name: 'employeeRole',
+                message: "What is the Employee's Role?",
+                type: 'list',
+                choices: results.map((role) => {
+                    return {
+                        name: `${role.id}: ${role.title}`,
+                        value: role
+                    }
+                })
+            }])
+            .then(({ firstName, lastName, employeeRole }) => {
+                connection.query('SELECT id, first_name, last_name FROM employee WHERE (id IN (SELECT manager_id FROM employee));', (err, results) => {
+                    err ? console.error(err) : inquirer.prompt([{
+                        name: 'Manager',
+                        type: 'list',
+                        choices: results.map((management) => {
+                            return {
+                                name: `${management.id}: ${management.last_name}, ${management.first_name}`,
+                                value: management
+                            }
+                        })
+                    }]).then(({ manager }) => {
+                        connection.query(
+                            `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${employeeRole.id}, ${manager.id});`, (err, results) => {
+                                err ? console.error(err) : console.log('\nYou have successfully added a new employee!');
+                                console.table(results);
+                                viewEmployees();
+                            })
+                        SystemSearch();
+                    });
+                });
+            });
+    });
+}
+
+const addRole = () => {
+    inquirer.prompt([{
+            name: 'title',
+            type: 'input',
+            message: 'Enter the new role title:',
+        }, {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the new role salary:',
+        }, {
+            name: 'department_id',
+            type: 'input',
+            message: 'Enter the new role department id:',
+        }])
+        .then(({ title, salary, department_id }) => {
+            const query = connection.query('INSERT INTO role SET ?', { title, salary, department_id }, (err, result) => {
+                err ? console.error(err) : console.table(results);
+                viewDepartments();
+            })
+        })
+};
+
+const updateEmployee = () => {
+    connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.id, role.title FROM employee LEFT JOIN role ON employee.role_id = role.id', (err, result) => {
+        if (err) throw err;
+        console.table(result);
+
+        inquirer.prompt([{
+            name: "id",
+            message: "Select the id of the employee to be updated: ",
+            type: "input",
+        }, {
+            name: "role_id",
+            message: "What role id will they be updating too: ",
+            type: "input",
+        }, {
+            name: "update_role",
+            message: "Which role to update?",
+            type: "list",
+            choices: [
+                'Legal Manager',
+                'Lawyer',
+                'Legal',
+                'Engineering Manager',
+                'Software Engineer',
+                'Engineer',
+                'Financial Manager',
+                'Financial Analyst',
+                'Analyst',
+                'Sales Manager',
+                'Sales Lead',
+                'Sales',
+            ],
+        }, ]).then(({ role_id, id }) => {
+            connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [role_id, id], (err, result) => {
+                if (err) throw err;
+                viewEmployees();
+            })
+        })
+    })
+};
